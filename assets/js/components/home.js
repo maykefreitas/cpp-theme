cf3c8f5f29affe3023c0b1bb30bb625fc7bb1717
Mayke <mayke_sargento@hotmail.com>
2017-05-26 01:55:15 -0300

    adding home videos

--- assets/js/components/home.js
+++ assets/js/components/home.js
@@ -9,7 +9,24 @@
       autoplayTimeout: 7000,
       dots: false,
       nav: false
-    })
+    });
+
+    $('.js-hero-videos-slider').owlCarousel({
+      items: 3,
+      loop: true,
+      dots: true,
+      nav: false,
+      margin: 5
+    });
+
+    $('.js-hero-videos .js-videos-nav a').on('click', function (e) {
+      e.preventDefault();
+
+      var $el = $(this);
+      var $html = $($el.data('html'));
+
+      $('.js-hero-videos .js-video-container').html($html);
+    });
   }
 
 })();
